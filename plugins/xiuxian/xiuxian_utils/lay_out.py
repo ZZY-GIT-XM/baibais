fb063b869f6ac38b872e77d3f7ef27c5c8630d87
ZZY-GIT-XM <824196365@qq.com>
2024-10-01 22:17:33 +0800

    剔除 重复定时任务

--- plugins/xiuxian/xiuxian_utils/lay_out.py
+++ plugins/xiuxian/xiuxian_utils/lay_out.py
@@ -10,61 +10,68 @@ from nonebot.matcher import Matcher
 from nonebot.params import Depends
 from nonebot.adapters.onebot.v11.event import MessageEvent, GroupMessageEvent
 from nonebot.adapters.onebot.v11 import Bot, MessageSegment
+
 from ..xiuxian_config import XiuConfig
 from .xiuxian2_handle import XiuxianDateManage
 from .utils import get_msg_pic
 
-
 sql_message = XiuxianDateManage()
-
-limit_all_message = require("nonebot_plugin_apscheduler").scheduler
-limit_all_stamina = require("nonebot_plugin_apscheduler").scheduler
-auto_recover_hp = require("nonebot_plugin_apscheduler").scheduler
-
-limit_all_data: Dict[str, Any] = {}
 limit_num = 99999
-max_stamina = XiuConfig().max_stamina
-stamina_recovery_rate = 1
 
-@auto_recover_hp.scheduled_job('interval', minutes=1)
-def auto_recover_hp_():
-    sql_message.auto_recover_hp()
+reset_xiaoxizidian = require("nonebot_plugin_apscheduler").scheduler
+limit_all_data: Dict[str, Any] = {}
 
-@limit_all_message.scheduled_job('interval', minutes=1)
-def limit_all_message_():
+
+@reset_xiaoxizidian.scheduled_job('interval', minutes=1)
+def reset_xiaoxizidian_():
     """重置消息字典"""
     global limit_all_data
-    limit_all_data  = {}
+    limit_all_data = {}
     logger.opt(colors=True).success(f"<green>已重置消息字典！</green>")
 
-@limit_all_stamina.scheduled_job('interval', minutes=XiuConfig().tilihuifu_min)
-def limit_all_stamina_():
-    """恢复体力，1分钟回一点"""
-    sql_message.update_all_users_stamina(max_stamina, stamina_recovery_rate)
 
 def limit_all_run(user_id: str):
+    """
+    limit_all_run 方法用于限制用户在一定时间内可以执行某项操作的次数。
+    它维护了一个全局字典 limit_all_data 来记录每个用户的使用次数。
+    当用户的使用次数超过预设的限制次数 limit_num 时，会返回不同的状态值。
+    """
+    # 声明全局变量 limit_all_data，确保可以在函数内部修改它
     global limit_all_data
+
+    # 确保 user_id 是字符串类型
     user_id = str(user_id)
+
+    # 初始化 num 和 tip 为 None，它们会在后续的逻辑中被赋值
     num = None
     tip = None
+
+    # 尝试从全局字典中获取用户的使用次数和提示状态
     try:
         num = limit_all_data[user_id]["num"]
         tip = limit_all_data[user_id]["tip"]
-    except:
-        limit_all_data[user_id] = {"num": 0,
-                                   "tip" : False}
+    except KeyError:
+        # 如果用户第一次访问，则初始化他们的使用次数为 0，提示状态为 False
+        limit_all_data[user_id] = {"num": 0, "tip": False}
         num = 0
         tip = False
-    num += 1    
+
+    # 增加用户的使用次数
+    num += 1
+
+    # 检查是否超过了使用次数的限制
     if num > limit_num and tip == False:
+        # 如果之前没有提示过，设置提示状态为 True，并返回 True 表示需要提示用户
         tip = True
         limit_all_data[user_id]["num"] = num
         limit_all_data[user_id]["tip"] = tip
         return True
-    if num > limit_num and tip == True:
+    elif num > limit_num and tip == True:
+        # 如果之前已经提示过，则直接返回 False 表示不需要再次提示
         limit_all_data[user_id]["num"] = num
         return False
     else:
+        # 如果没有超过限制次数，则更新用户的使用次数，并返回 None 表示一切正常
         limit_all_data[user_id]["num"] = num
         return None
 
@@ -83,7 +90,7 @@ def format_time(seconds: int) -> str:
         return f"{minutes}分钟{seconds}秒"
     else:
         return f"{seconds}秒"
-    
+
 
 def get_random_chat_notice():
     return random.choice([
@@ -92,7 +99,8 @@ def get_random_chat_notice():
         "时间还没到，还有{}，歇会歇会~~"
     ])
 
-bu_ji_notice = random.choice(["别急！","急也没用!","让我先急!"])
+
+bu_ji_notice = random.choice(["别急！", "急也没用!", "让我先急!"])
 
 
 class CooldownIsolateLevel(IntEnum):
@@ -102,6 +110,7 @@ class CooldownIsolateLevel(IntEnum):
     GROUP = auto()
     USER = auto()
     GROUP_USER = auto()
+
 
 def Cooldown(
         cd_time: float = 0.5,
@@ -133,7 +142,6 @@ def Cooldown(
         )
     running: DefaultDict[str, int] = defaultdict(lambda: parallel)
     time_sy: Dict[str, int] = {}
-    
 
     def increase(key: str, value: int = 1):
         running[key] += value
@@ -183,9 +191,11 @@ def Cooldown(
                 ):
                     bot = await assign_bot_group(group_id=group_id)
                     if at_sender:
-                        await bot.send(event=event, message=MessageSegment.at(event.get_user_id()) + "本群已关闭修仙模组,请联系管理员开启,开启命令为【启用修仙功能】!")
+                        await bot.send(event=event, message=MessageSegment.at(
+                            event.get_user_id()) + "本群已关闭修仙模组,请联系管理员开启,开启命令为【启用修仙功能】!")
                     else:
-                        await bot.send(event=event, message="本群已关闭修仙模组,请联系管理员开启,开启命令为【启用修仙功能】!")
+                        await bot.send(event=event,
+                                       message="本群已关闭修仙模组,请联系管理员开启,开启命令为【启用修仙功能】!")
                     await matcher.finish()
                 else:
                     await matcher.finish()
@@ -193,7 +203,7 @@ def Cooldown(
                 pass
         if stamina_cost > 0:
             user_data = sql_message.get_user_info_with_id(user_id)
-            if user_data :
+            if user_data:
                 if user_data['user_stamina'] < stamina_cost:
                     msg = "你没有足够的体力，请等待体力恢复后再试！"
                     await bot.send_group_msg(group_id=int(group_id), message=msg)
@@ -206,13 +216,15 @@ def Cooldown(
                     time = 1
                 formatted_time = format_time(time)
                 if XiuConfig().img:
-                    pic = await get_msg_pic(f"@{event.sender.nickname}\n" + get_random_chat_notice().format(formatted_time))
+                    pic = await get_msg_pic(
+                        f"@{event.sender.nickname}\n" + get_random_chat_notice().format(formatted_time))
                     bot = await assign_bot_group(group_id=group_id)
                     await bot.send_group_msg(group_id=int(group_id), message=MessageSegment.image(pic))
                     await matcher.finish()
                 else:
                     bot = await assign_bot_group(group_id=group_id)
-                    await bot.send_group_msg(group_id=int(group_id), message=get_random_chat_notice().format(formatted_time))
+                    await bot.send_group_msg(group_id=int(group_id),
+                                             message=get_random_chat_notice().format(formatted_time))
                     await matcher.finish()
             else:
                 await matcher.finish()
